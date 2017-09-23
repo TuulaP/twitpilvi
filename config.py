625acfb8c4a3659e0afa83f92dbd7c960d3dbc0b
Tuula P <tuula.paakkonen@helsinki.fi>
2017-09-23 17:52:03 +0300

    :newspaper:

---
+++ config.py
@@ -0,0 +1,12 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+# consumer_key = "XxXxXxxXXXxxxxXXXxXX"
+# consumer_secret = "xXXXXXXXXxxxxXxXXxxXxxXXxXxXxxxxXxXXxxxXXx"
+# access_key = "XXXXXXXX-xxXXxXXxxXxxxXxXXxXxXxXxxxXxxxxXxXXxXxxXX"
+# access_secret = "XxXXXXXXXXxxxXXXxXXxXxXxxXXXXXxXxxXXXXx"
+
+
